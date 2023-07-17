a4f28d888853df14d290f0eb27726f5d95a3b6cb
Parv Shah <parvthecoder@gmail.com>
2023-07-17 21:40:51 +0530

    first commit

---
+++ JavaBasics5_largestof2.java
@@ -0,0 +1,15 @@
+public class JavaBasics5_largestof2{
+    public static void main(String args[])
+    {
+        int A=5;
+        int B=10;
+        if ( A>B )
+        {
+            System.out.println("A is the Largest");
+        }
+        else
+        {
+            System.out.println("B is the Largest");
+        }
+    }
+}
