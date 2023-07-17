53a1287735b057480fe426cfd34341eff18cc0ca
Parv Shah <parvthecoder@gmail.com>
2023-07-17 22:34:44 +0530

    commit

---
+++ JavaBasics9_Largestof3.java
@@ -0,0 +1,20 @@
+public class JavaBasics9_Largestof3 {
+    public static void main(String args[])
+    {
+        int a=5;
+        int b=10;
+        int c=1;
+        if ((a>b) && (a>c))
+        {
+            System.out.println("A is the largest.");
+        }
+        else if ( b>c)
+        {
+            System.out.println("B is the largest.");
+        }
+        else
+        {
+            System.out.println("C is the largest.");
+        }
+    }
+}
