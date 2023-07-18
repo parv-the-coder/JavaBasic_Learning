243601221167c48cf0556f1bdf82b2e86fc95760
Parv Shah <parvthecoder@gmail.com>
2023-07-18 23:49:52 +0530

    initial commit

---
+++ JavaBasics18_Reverse_no.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class JavaBasics18_Reverse_no {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int rev=0;
+        while(n!=0)
+        {
+            int lastdigit = n%10;
+            rev= rev*10 + lastdigit;
+            n/=10;
+        }
+        System.out.println(rev);
+        sc.close();
+    }
+}
