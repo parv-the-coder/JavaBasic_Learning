6323a47cf34b3bc89c9cd378cf07f72d789556b9
Parv Shah <parvthecoder@gmail.com>
2023-07-18 22:13:31 +0530

    first commit
    While LOOP

---
+++ JavaBasics15_1to10.java
@@ -0,0 +1,13 @@
+public class JavaBasics15_1to10 {
+
+    public static void main(String args[])
+    {
+        int counter=1;
+        while(counter<11)
+        {
+            System.out.println(counter);
+            counter++;
+        }
+    }
+    
+}
