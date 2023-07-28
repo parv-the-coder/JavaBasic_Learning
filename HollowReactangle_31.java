f1e012950b7d9272c3f94fab8923008ec9613aa2
Parv Shah <parvthecoder@gmail.com>
2023-07-29 00:42:52 +0530

    initial commit

---
+++ HollowReactangle_31.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class HollowReactangle_31 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Rows:");
+        int rows = sc.nextInt();
+        System.out.print("Enter cols:");
+        int cols = sc.nextInt();
+        for (int i = 0; i <= rows; i++) {
+            for (int j = 0; j <= cols; j++) {
+                if (i == 0 || i == rows || j == 0 || j == cols) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+
+    }
+}
